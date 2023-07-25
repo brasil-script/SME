05fa86cf98a2702d7afcdccfa6a1616aff443128
brasil-script <119768524+brasil-script@users.noreply.github.com>
2023-07-25 13:31:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,415 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  /*DROP-BOX PAG03*/ 
+
+  function myFunction2() {
+    document.getElementById("myDropdown2pag03").classList.toggle("show2pag03");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn2pag03')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content2pag03");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show2pag03')) {
+          openDropdown.classList.remove('show2pag03');
+        }
+      }
+    }
+  }
+  
+
+
+  function myFunction3() {
+    document.getElementById("myDropdown3pag03").classList.toggle("show3pag03");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn3pag03')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content3pag03");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show3pag03')) {
+          openDropdown.classList.remove('show3pag03');
+        }
+      }
+    }
+  }
+  
+
+  
+  function myFunction4() {
+    document.getElementById("myDropdown4pag03").classList.toggle("show4pag03");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn4pag03')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content4pag03");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show4pag03')) {
+          openDropdown.classList.remove('show4pag03');
+        }
+      }
+    }
+  }
+  
+
+    
+  function myFunction5() {
+    document.getElementById("myDropdown5pag03").classList.toggle("show5pag03");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn5pag03')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content5pag03");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show5pag03')) {
+          openDropdown.classList.remove('show5pag03');
+        }
+      }
+    }
+  }
+  
+
+  
+    
+  function myFunction6() {
+    document.getElementById("myDropdown6pag03").classList.toggle("show6pag03");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn6pag03')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content6pag03");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show6pag03')) {
+          openDropdown.classList.remove('show6pag03');
+        }
+      }
+    }
+  }
+  
+      
+  function myFunction7() {
+    document.getElementById("myDropdown7pag03").classList.toggle("show7pag03");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn7pag03')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content7pag03");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show7pag03')) {
+          openDropdown.classList.remove('show7pag03');
+        }
+      }
+    }
+  }
+
+
+  function myFunction8() {
+    document.getElementById("myDropdown8pag03").classList.toggle("show8pag03");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn8pag03')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content8pag03");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show8pag03')) {
+          openDropdown.classList.remove('show8pag03');
+        }
+      }
+    }
+  }
+
+  function myFunction9() {
+    document.getElementById("myDropdown9pag03").classList.toggle("show9pag03");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn9pag03')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content9pag03");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show9pag03')) {
+          openDropdown.classList.remove('show9pag03');
+        }
+      }
+    }
+  }
+
+  
+  function myFunction10() {
+    document.getElementById("myDropdown10pag03").classList.toggle("show10pag03");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn10pag03')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content10pag03");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show10pag03')) {
+          openDropdown.classList.remove('show10pag03');
+        }
+      }
+    }
+  }
+
+  function myFunction11() {
+    document.getElementById("myDropdown11pag03").classList.toggle("show11pag03");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn11pag03')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content11pag03");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show11pag03')) {
+          openDropdown.classList.remove('show11pag03');
+        }
+      }
+    }
+  }
+
+  function myFunction12() {
+    document.getElementById("myDropdown12pag03").classList.toggle("show12pag03");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn12pag03')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content12pag03");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show12pag03')) {
+          openDropdown.classList.remove('show12pag03');
+        }
+      }
+    }
+  }
+
+  function myFunction13() {
+    document.getElementById("myDropdown13pag03").classList.toggle("show13pag03");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn13pag03')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content13pag03");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show13pag03')) {
+          openDropdown.classList.remove('show13pag03');
+        }
+      }
+    }
+  }
+
+  function myFunction14() {
+    document.getElementById("myDropdown14pag03").classList.toggle("show14pag03");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn14pag03')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content14pag03");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show14pag03')) {
+          openDropdown.classList.remove('show14pag03');
+        }
+      }
+    }
+  }
+
+  
+  function myFunction15() {
+    document.getElementById("myDropdown15pag03").classList.toggle("show15pag03");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn15pag03')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content15pag03");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show15pag03')) {
+          openDropdown.classList.remove('show15pag03');
+        }
+      }
+    }
+  }
+
+  function myFunction16() {
+    document.getElementById("myDropdown16pag03").classList.toggle("show16pag03");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn16pag03')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content16pag03");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show16pag03')) {
+          openDropdown.classList.remove('show16pag03');
+        }
+      }
+    }
+  }
+
+  function myFunction17() {
+    document.getElementById("myDropdown17pag03").classList.toggle("show17pag03");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn17pag03')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content17pag03");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show17pag03')) {
+          openDropdown.classList.remove('show17pag03');
+        }
+      }
+    }
+  }
+
+  function myFunction18() {
+    document.getElementById("myDropdown18pag03").classList.toggle("show18pag03");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn18pag03')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content18pag03");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show18pag03')) {
+          openDropdown.classList.remove('show18pag03');
+        }
+      }
+    }
+  }
+
+  function myFunction19() {
+    document.getElementById("myDropdown19pag03").classList.toggle("show19pag03");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn19pag03')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content19pag03");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show19pag03')) {
+          openDropdown.classList.remove('show19pag03');
+        }
+      }
+    }
+  }
+
+
+  
+  function myFunction20() {
+    document.getElementById("myDropdown20pag03").classList.toggle("show20pag03");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn20pag03')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content20pag03");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show20pag03')) {
+          openDropdown.classList.remove('show20pag03');
+        }
+      }
+    }
+  }
+
+
+    
+  function myFunction21() {
+    document.getElementById("myDropdown21pag03").classList.toggle("show21pag03");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn21pag03')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content21pag03");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show21pag03')) {
+          openDropdown.classList.remove('show21pag03');
+        }
+      }
+    }
+  }
+
+      
+  function myFunction22() {
+    document.getElementById("myDropdown22pag03").classList.toggle("show22pag03");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn22pag03')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content22pag03");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show22pag03')) {
+          openDropdown.classList.remove('show22pag03');
+        }
+      }
+    }
+  }
